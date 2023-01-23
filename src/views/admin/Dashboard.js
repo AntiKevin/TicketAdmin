3edb128f8f2b0f1c0d14191c714a56ebbe500b2e
Antikevin <kevin_3467@yahoo.com>
2023-01-22 22:36:31 -0300

    refactor: :construction: temp remove graph

--- src/views/admin/Dashboard.js
+++ src/views/admin/Dashboard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 // components
 
-import CardBarChart from "components/Cards/CardBarChart.js";
+//import CardBarChart from "components/Cards/CardBarChart.js";
 import CardPageVisits from "components/Cards/CardPageVisits.js";
 import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
 
